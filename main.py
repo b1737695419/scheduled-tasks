86e6eb8c18b88dafdbdf769a12ebea7cf27aed8a
b1737695419 <b1737695419@gmail.com>
2026-07-26 21:50:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,40 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
-import pandas
-import random
 import smtplib
+import datetime as dt
+import pandas as pd
+import random
 import os
 
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+now=dt.datetime.now()
+today_day=now.day
+today_month=now.month
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+todayM_D=(today_month,today_day)
+
+
+with open("birthdays.csv") as birthdays_file:
+    birthdays = pd.read_csv(birthdays_file)
+
+
+dict_birthdays =  {(row["month"],row["day"]):row for (index, row) in birthdays.iterrows()}
+
+if todayM_D in dict_birthdays:
+
+    with open(f"letter_templates/letter_{random.randint(1, 3)}.txt") as letter_template:
+        letter_template = letter_template.read()
+        letter_template=letter_template.replace("[NAME]",f"{dict_birthdays[todayM_D]['name']}")
+        print(letter_template)
+    #HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
+    # if (today_month, today_day) in birthdays_dict:
+    with smtplib.SMTP("smtp.gmail.com", 587) as user:
+        user.starttls()
+        user.login("MY_EMAIL", "MY_PASSWORD")
+        user.sendmail(from_addr="MY_EMAIL",to_addrs=f"{dict_birthdays[todayM_D]['email']}"
+                      ,msg=f"subject:happy birthday\n\n{letter_template}")
